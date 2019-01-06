a71d47d6a70a680b62fbc90d0623ae8d5b8519f5
Mareeshub <marees2910@gmail.com>
2019-01-06 10:55:18 -0500

    updated new files

---
+++ src/main/java/org/simplilearn/maven/simplemaven/App.java
@@ -0,0 +1,13 @@
+package org.simplilearn.maven.simplemaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
